ca19da89c6b0da0ae014582782f86f4129c635e9
Efraín Muñoz Coreano <munozcoreano@gmail.com>
2018-01-19 14:11:04 -0400

    Trades endpoint not yet working.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/efrainmunoz/generic-api/orderbook"
 	"github.com/gorilla/mux"
 	"net/http"
+	"github.com/efrainmunoz/generic-api/trades"
 )
 
 // MAIN
@@ -18,12 +19,18 @@ func main() {
 	go orderbook.InitState()
 	go orderbook.InitService()
 
+	// Trades
+	go trades.InitState()
+	go trades.InitService()
+
 	// Set api routes
 	router := mux.NewRouter()
 	router.HandleFunc("/ticker", ticker.GetAll).Methods("GET")
 	router.HandleFunc("/ticker/{pair}", ticker.Get).Methods("GET")
 	router.HandleFunc("/orderbook", orderbook.GetAll).Methods("GET")
 	router.HandleFunc("/orderbook/{pair}", orderbook.Get).Methods("GET")
+	router.HandleFunc("trades", trades.GetAll).Methods("GET")
+	router.HandleFunc("trades/{pair}", trades.Get).Methods("GET")
 
 	// Start the server
 	http.ListenAndServe(":8000", router)
